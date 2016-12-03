b541a98e410bf7ed6066922ed05429b211654f41
Gamerlazer <gamerlazer@gmail.com>
2016-12-03 13:44:18 -0800

    Complete noTimeForATaxiCab for correct answer

--- noTimeForATaxicab.js
+++ noTimeForATaxicab.js
@@ -30,7 +30,9 @@ const taxiCab = (input) => {
   let currentCoordinate = [0, 0];
   let facing = 'north';
   let instructions = [];
-  input.split(',').forEach(direction => instructions.push(direction.replace(' ', '')))
+  input.split(',').forEach(direction => {
+    instructions.push(direction.replace(' ', ''))
+  })
 
   let north = {
     R: (coordinate, steps) => coordinate[0] += steps,
@@ -61,8 +63,7 @@ const taxiCab = (input) => {
 
   let returnCoordinate = (facing, coordinate, instruction) => {
     let direction = instruction.split('')[0];
-    let steps = +instruction.split('')[1];
-
+    let steps = +instruction.split('').splice(1).join('');
     directions[facing][direction](coordinate, steps);
 
     return coordinate;
@@ -106,6 +107,7 @@ const taxiCab = (input) => {
 }
 
 console.log(taxiCab('R2, L3'), 5)
-console.log(taxiCab('R2, R2, R2 '), 2)
-console.log(taxiCab('R5, L5, R5, R3'), 12)
-console.log(taxiCab('R2, L3, R2, R4, L2, L1, R2, R4, R1, L4, L5, R5, R5, R2, R2, R1, L2, L3, L2, L1, R3, L5, R187, R1, R4, L1, R5, L3, L4, R50, L4, R2, R70, L3, L2, R4, R3, R194, L3, L4, L4, L3, L4, R4, R5, L1, L5, L4, R1, L2, R4, L5, L3, R4, L5, L5, R5, R3, R5, L2, L4, R4, L1, R3, R1, L1, L2, R2, R2, L3, R3, R2, R5, R2, R5, L3, R2, L5, R1, R2, R2, L4, L5, L1, L4, R4, R3, R1, R2, L1, L2, R4, R5, L2, R3, L4, L5, L5, L4, R4, L2, R1, R1, L2, L3, L2, R2, L4, R3, R2, L1, L3, L2, L4, L4, R2, L3, L3, R2, L4, L3, R4, R3, L2, L1, L4, R4, R2, L4, L4, L5, L1, R2, L5, L2, L3, R2, L2'))
+// console.log(taxiCab('R2, R2, R2 '), 2)
+// console.log(taxiCab('R5, L5, R5, R3'), 12)
+// console.log(taxiCab('R2, L3, R2, R4, L2, L1, R2, R4, R1, L4, L5, R5, R5, R2, R2, R1, L2, L3, L2, L1, R3, L5, R187, R1, R4, L1, R5, L3, L4, R50, L4, R2, R70, L3, L2, R4, R3, R194, L3, L4, L4, L3, L4, R4, R5, L1, L5, L4, R1, L2, R4, L5, L3, R4, L5, L5, R5, R3, R5, L2, L4, R4, L1, R3, R1, L1, L2, R2, R2, L3, R3, R2, R5, R2, R5, L3, R2, L5, R1, R2, R2, L4, L5, L1, L4, R4, R3, R1, R2, L1, L2, R4, R5, L2, R3, L4, L5, L5, L4, R4, L2, R1, R1, L2, L3, L2, R2, L4, R3, R2, L1, L3, L2, L4, L4, R2, L3, L3, R2, L4, L3, R4, R3, L2, L1, L4, R4, R2, L4, L4, L5, L1, R2, L5, L2, L3, R2, L2'))
+console.log(taxiCab('R2, L1, R2, R1, R1, L3, R3, L5, L5, L2, L1, R4, R1, R3, L5, L5, R3, L4, L4, R5, R4, R3, L1, L2, R5, R4, L2, R1, R4, R4, L2, L1, L1, R190, R3, L4, R52, R5, R3, L5, R3, R2, R1, L5, L5, L4, R2, L3, R3, L1, L3, R5, L3, L4, R3, R77, R3, L2, R189, R4, R2, L2, R2, L1, R5, R4, R4, R2, L2, L2, L5, L1, R1, R2, L3, L4, L5, R1, L1, L2, L2, R2, L3, R3, L4, L1, L5, L4, L4, R3, R5, L2, R4, R5, R3, L2, L2, L4, L2, R2, L5, L4, R3, R1, L2, R2, R4, L1, L4, L4, L2, R2, L4, L1, L1, R4, L1, L3, L2, L2, L5, R5, R2, R5, L1, L5, R2, R4, R4, L2, R5, L5, R5, R5, L4, R2, R1, R1, R3, L3, L3, L4, L3, L2, L2, L2, R2, L1, L3, R2, R5, R5, L4, R3, L3, L4, R2, L5, R5'))
