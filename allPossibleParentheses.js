500bfb905cdf78e09c3b84951311eff095f85c20
Gamerlazer <gamerlazer@gmail.com>
2016-12-14 18:45:49 -0800

    Remove excess code from allPossibleParentheses

--- allPossibleParentheses.js
+++ allPossibleParentheses.js
@@ -15,13 +15,6 @@ const allPossibleParentheses = (n) => {
       }
       return results.push(currentString);
     }
-    if (currentString.length === 2 * n) {
-      if (remainingOpenBraces === 0 && remainingCloseBraces === 0) {
-        return results.push(currentString);
-      } else {
-        return;
-      }
-    }
     generatePossibleParentheses(currentString + '{', remainingOpenBraces - 1, remainingCloseBraces + 1);
     generatePossibleParentheses(currentString + '}', remainingOpenBraces, remainingCloseBraces - 1);
   }
@@ -29,4 +22,30 @@ const allPossibleParentheses = (n) => {
   return results;
 }
 
-console.log(allPossibleParentheses(5));
+// console.log(allPossibleParentheses(3));
+
+// let print_all_parentheses_rec = function(n, left_count, right_count, output) {
+
+//   if (left_count >= n && right_count >= n) {
+//     console.log(output);
+//   }
+
+//   if (left_count < n) {
+//     output.push('{');
+//     print_all_parentheses_rec(n, left_count + 1, right_count, output);
+//     output.pop();
+//   }
+//   if (right_count < left_count) {
+//     output.push('}');
+//     print_all_parentheses_rec(n, left_count, right_count + 1, output);
+//     output.pop();
+//   }
+// };
+
+// let print_all_parentheses = function(n) {
+//   let output = [];
+//   print_all_parentheses_rec(n, 0, 0, output);
+//   return output;
+// };
+
+// console.log(print_all_parentheses(3))
